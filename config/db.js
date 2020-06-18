ccc2f8923651359401420129cffc5009d019ccbb
Soundaryabs <61044907+Soundaryabs@users.noreply.github.com>
2020-06-18 11:40:34 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 //creating module to handle db connection
 
 module.exports ={
-    DB:"mongodb://localhost:27017/DB_FoodApp", //this is the local connection of DB
+    //DB:"mongodb://localhost:27017/DB_FoodApp", //this is the local connection of DB
+    DB:"mongodb+srv://Soundarya:Burger@60@cluster0-d4scp.mongodb.net/DB_FoodApp?retryWrites=true&w=majority"
 };
