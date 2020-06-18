9e81c2182c422d65e39a9fc302e275840376aedb
Soundaryabs <61044907+Soundaryabs@users.noreply.github.com>
2020-06-18 11:20:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const bodyParser = require("body-parser");
-const PORT = 4000;
+const PORT =Number(process.env.PORT || 5000) ;
 
 const cors = require("cors");
 const mongoose = require("mongoose");
